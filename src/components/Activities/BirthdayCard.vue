0d6a3ce8823155bab742d3c2db3b8eb1b885acaf
Martyna227 <gradzkamartyna@gmail.com>
2023-01-10 18:39:54 +0100

    birthdaycard

---
+++ src/components/Activities/BirthdayCard.vue
@@ -0,0 +1,22 @@
+<template>
+    <article class="flex">
+        <h3>{{ h3 }}</h3>
+        <h4> {{h4}}</h4>
+        <time>{{ time}}</time>
+        <p>{{minutes}}</p>
+        <p>{{date}}</p>
+        <p>{{price}}</p>
+
+    </article>
+</template>
+
+<script>
+export default {
+    props: ["h3", "h4", "time", "price", "date", "minutes"]
+}
+</script>
+<style scoped>
+article {
+    flex-direction:column
+}
+</style>
